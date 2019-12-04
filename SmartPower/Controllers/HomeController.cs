5828707de993f7b00f008a39320a6ee4c561a592
Mohammed <mido5219@gmail.com>
2019-12-05 00:07:02 +0200

    small naming change for rfid actions

--- SmartPower/Controllers/HomeController.cs
+++ SmartPower/Controllers/HomeController.cs
@@ -36,67 +36,6 @@ namespace SmartPower.Controllers
         }
 
 
-        //public async Task<string> Update()
-        //{
-        //    var query = HttpContext.Request.Query;
-        //    var values = query.Where(o => o.Key.StartsWith("p")).ToDictionary(o => o.Key);
-        //    Console.WriteLine("Recieved");
-        //    Reading reading = new Reading()
-        //    {
-
-        //        MachineCode = Convert.ToString(values["pMC"].Value),
-        //        Length = Convert.ToDecimal(values["pLength"].Value),
-
-        //        status = Convert.ToInt32(values["pST"].Value),
-
-        //        time = DateTime.Now,
-        //    };
-        //    //machine starting 
-        //    if (reading.Length == 0 && reading.status == 1)
-        //    {
-        //        Console.WriteLine("Status is 1");
-
-        //        var jobOrders = DB.jobOrders.Where(j => j.MachineCode == Convert.ToString(values["pMC"].Value)).ToList();
-
-        //        if (jobOrders.Any())
-        //        {
-        //            Console.WriteLine("job orders already exist");
-        //            reading.JobOrders = jobOrders;
-        //            DB.SaveChanges();
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("getting and adding job orders");
-        //            AddJobOrder(getJob(Convert.ToString(values["pMC"].Value)),reading);
-        //            reading.JobOrders = jobOrders;
-        //        }
-        //    }
-        //    else if (reading.Length != 0 && reading.status == 0)
-        //    {
-        //        Console.WriteLine("Status is 0 complete");
-        //        var jobOrders = DB.jobOrders.Where(j => j.MachineCode == Convert.ToString(values["pMC"].Value) && j.TotalLength == 0);
-
-        //        reading.status = 0;
-        //        await jobOrders.ForEachAsync(j =>
-        //        {
-        //            j.EndDate = DateTime.Now;
-        //            j.TotalLength = reading.Length;
-        //        });
-        //        reading.JobOrders = jobOrders.ToList();
-        //        DB.jobOrders.UpdateRange(jobOrders);
-        //        DB.SaveChanges();
-
-        //    }//else if(encoder.Length != 0 && encoder.status == 2)
-        //     //{
-
-        //    //}
-
-
-        //    DB.Reading.Add(reading);
-        //    //DB.re.Add(encoder);
-        //    DB.SaveChanges();
-        //    return $"ok";
-        //}
         public IActionResult Index()
         {
             return RedirectToAction("Data");
@@ -190,7 +129,7 @@ namespace SmartPower.Controllers
         }
 
         [HttpGet]
-        public IActionResult ConfirmFinished([FromQuery] string PairId, [FromQuery] string MachineId, [FromQuery] int LineId)
+        public IActionResult GetLength([FromQuery] string PairId, [FromQuery] string MachineId, [FromQuery] int LineId)
         {
 
             Reading reading = _context.Readings.FirstOrDefault(r => r.PairId == PairId && r.MachineId == MachineId && r.LineId == LineId && r.Status == 0 && r.Assignment != 1);
@@ -248,7 +187,7 @@ namespace SmartPower.Controllers
             }
         }
         [HttpGet]
-        public IActionResult ConfirmERP([FromQuery] string PairId, [FromQuery] string MachineId, [FromQuery] int LineId, [FromQuery] short Flag)
+        public IActionResult ConfirmFinished([FromQuery] string PairId, [FromQuery] string MachineId, [FromQuery] int LineId, [FromQuery] short Flag)
         {
             Reading reading = _context.Readings.FirstOrDefault(r => r.PairId == PairId && r.MachineId == MachineId && r.LineId == LineId && r.Status == 0);
             if (reading.Assignment == 1 || reading.Assignment == 0) // will he confirm after reading or confirm instantly
@@ -287,43 +226,6 @@ namespace SmartPower.Controllers
 
         }
 
-
-        //takes the list of job orders and assigns each one to the machine (machine code)
-        //public void AddJobOrder(ICollection<string> Jobs, Reading reading)
-        //{
-        //    var JobList = Jobs.ToList();
-        //    DateTime date = DateTime.Now;
-        //    Console.WriteLine("About to print JOBS before ADDING");
-        //    JobList.ForEach(j =>
-        //    {
-        //        JobOrder jobOrder = new JobOrder()
-        //        {
-
-        //            JobOrderId = j,
-        //            StartDate = date,
-        //            EndDate = null,
-        //            MachineCode = reading.MachineCode,
-        //            TotalLength = 0
-        //        };
-        //        DB.jobOrders.Add(jobOrder);
-        //    });
-        //    DB.SaveChanges();
-
-        //    //DB.jobOrders.AddRange(jobOrders);
-        //    //readings.ForEachAsync(r => r.jobOrders = jobOrders);
-        //    //DB.SaveChangesAsync();
-
-
-        //}
-
-        //public void UpdateJobOrder(ICollection <JobOrder> orders, decimal length)
-        //{
-        //    JobOrder jobOrder = DB.jobOrders.FirstOrDefault(j => j.JobOrderId == jobId);
-        //    jobOrder.EndDate = DateTime.Now;
-        //    jobOrder.TotalLength = length;
-        //    DB.jobOrders.Update(jobOrder);
-        //    DB.SaveChanges();
-        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
